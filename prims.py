7234797d2fa164ccebc85832f924d9e0d7a00f73
Varun Ingle <72153612+scorpion-varun@users.noreply.github.com>
2022-05-09 23:22:23 +0530

    Add files via upload

---
+++ prims.py
@@ -0,0 +1,52 @@
+import sys
+
+
+class Graph():
+
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = [[0 for column in range(vertices)]
+                      for row in range(vertices)]
+
+    def printMST(self, parent):
+        print("Edge\tWeight")
+        for i in range(1, self.V):
+            print(parent[i], "-", i, "\t", self.graph[i][parent[i]])
+
+    def minKey(self, key, mstSet):
+        min = sys.maxsize
+
+        for v in range(self.V):
+            if key[v] < min and mstSet[v] == False:
+                min = key[v]
+                min_index = v
+        return min_index
+
+    def primMST(self):
+        key = [sys.maxsize]*self.V
+        parent = [None]*self.V
+        key[0] = 0
+        mstSet = [False]*self.V
+
+        parent[0] = -1
+
+        for cout in range(self.V):
+            u = self.minKey(key, mstSet)
+            mstSet[u] = True
+            for v in range(self.V):
+                if self.graph[u][v] > 0 and mstSet[v] == False and key[v] > self.graph[u][v]:
+                    key[v] = self.graph[u][v]
+                    parent[v] = u
+
+        self.printMST(parent)
+
+
+g = Graph(5)
+g.graph = []
+n = int(input("Enter number of elemnts: "))
+for i in range(0, n):
+    ele = [int(input()), int(input()), int(
+        input()), int(input()), int(input())]
+    g.graph.append(ele)
+print(g.graph)
+g.primMST()
